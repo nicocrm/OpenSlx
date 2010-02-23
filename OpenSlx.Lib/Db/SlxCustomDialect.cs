0e10c7d1b858f18322a5786f8c576b49c6e3d178
Nicolas Galler <ngaller@gmail.com>
2010-02-23 12:15:24 -0600

    Initial

---
+++ OpenSlx.Lib/Db/SlxCustomDialect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenSlx.Lib.Db
+{
+    /// <summary>
+    /// A custom SalesLogix SQL dialect for NHibernate.
+    /// </summary>
+    public class SlxCustomDialect : 
+    {
+    }
+}
